532f9a9608e5718f037949f9e8d92eaec136494b
ARBUCHELI <radio_seattle@hotmail.com>
2024-05-10 12:18:26 +0300

    open-source middleware: open source

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 
@@ -24,24 +25,13 @@ const jellybeanBag = {
   }
 };
 
-const bodyParser = (req, res, next) => {
-  let queryData = '';
-  req.on('data', (data) => {
-    data = data.toString();
-    queryData += data;
-  });
-  req.on('end', () => {
-    if (queryData) {
-      req.body = JSON.parse(queryData);
-    }
-    next();
-  });
-};
-
 // Logging Middleware
 app.use(morgan('dev'));
 
-app.use('/beans/:beanName', (req, res, next) => {
+// Body parsing middleware
+app.use(bodyParser.json());
+
+app.use('/:beanName', (req, res, next) => {
   const beanName = req.params.beanName;
   if (!jellybeanBag[beanName]) {
     return res.status(404).send('Bean with that name does not exist');
@@ -55,7 +45,7 @@ app.get('/beans/', (req, res, next) => {
   res.send(jellybeanBag);
 });
 
-app.post('/beans/', bodyParser, (req, res, next) => {
+app.post('/beans/', (req, res, next) => {
   const body = req.body;
   const beanName = body.name;
   if (jellybeanBag[beanName] || jellybeanBag[beanName] === 0) {
@@ -72,13 +62,13 @@ app.get('/beans/:beanName', (req, res, next) => {
   res.send(req.bean);
 });
 
-app.post('/beans/:beanName/add', bodyParser, (req, res, next) => {
+app.post('/beans/:beanName/add', (req, res, next) => {
   const numberOfBeans = Number(req.body.number) || 0;
   req.bean.number += numberOfBeans;
   res.send(req.bean);
 });
 
-app.post('/beans/:beanName/remove', bodyParser, (req, res, next) => {
+app.post('/beans/:beanName/remove', (req, res, next) => {
   const numberOfBeans = Number(req.body.number) || 0;
   if (req.bean.number < numberOfBeans) {
     return res.status(400).send('Not enough beans in the jar to remove!');
@@ -93,7 +83,7 @@ app.delete('/beans/:beanName', (req, res, next) => {
   res.status(204).send();
 });
 
-app.put('/beans/:beanName/name', bodyParser, (req, res, next) => {
+app.put('/beans/:beanName/name', (req, res, next) => {
   const beanName = req.beanName;
   const newName = req.body.name;
   jellybeanBag[newName] = req.bean;
